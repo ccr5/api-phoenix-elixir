a244905c5db7049c98d669abe54b048dd7a8b935
ccr5 <matt-gomes@live.com>
2021-04-21 20:12:04 -0300

    implement facade design pattern

--- lib/phoenixapi.ex
+++ lib/phoenixapi.ex
@@ -1,9 +1,7 @@
 defmodule Phoenixapi do
-  @moduledoc """
-  Phoenixapi keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias Phoenixapi.Restaurants.Create
+  # this file is the module of my whole application, so to implement facade design pattern
+  # we delegate a fucntio to here then my controllers just need to know this module
+  # We can study more about facade design patterns and how it works
+  defdelegate create_restaurant(params), to: Create, as: :call
 end
