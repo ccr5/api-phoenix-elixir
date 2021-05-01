ebfb36d5a75cd8e6a9843947ddbb74e139d8174a
ccr5 <matt-gomes@live.com>
2021-04-30 23:55:15 -0300

    class 5

--- lib/phoenixapi/supplies/expiration_notification.ex
+++ lib/phoenixapi/supplies/expiration_notification.ex
@@ -5,11 +5,15 @@ defmodule Phoenixapi.Supplies.ExpirationNotification do
   def send do
     data = GetByExpiration.call()
 
-    Enum.each(data, fn {to_email, supplies} ->
-      to_email
-      |> ExpirationEmail.create(supplies)
-      # ! =  return an exception
-      |> Mailer.deliver_later!()
-    end)
+    data
+    |> Task.async_stream(fn {to_email, supplies} -> send_email(to_email, supplies) end)
+    |> Stream.run()
+  end
+
+  defp send_email(to_email, supplies) do
+    to_email
+    |> ExpirationEmail.create(supplies)
+    # ! =  return an exception
+    |> Mailer.deliver_later!()
   end
 end
